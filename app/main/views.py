440b01c24a42d259089265b1652b7c1cb782647e
marion vutagwa <marionvutagwa@yahoo.com>
2020-07-08 01:49:56 +0300

    add new changes

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..requests import get_sources,get_news,get_newss
+from ..requests import get_sources,get_article,get_articles
 
 # Views
 @main.route('/')
@@ -16,16 +16,16 @@ def index():
     else:
         return render_template('index.html', title = title,news = General_news)
 
-@main.route('/newss/<id>')
-def newss(id):
+@main.route('/articles/<id>')
+def articles(id):
     '''
-    view newss page function that return  newss detail page
+    view articles page function that return  articles detail page
     '''
-    newss = get_news(id)
-    title = f'(newss.title)'
-    cnn_news = get_newss('cnn') 
-    # BBC_News =get_newss('bbc_news')
-    return render_template('newss.html',title = title, newss = newss,cnn = cnn_news) 
+    articles = get_article(id)
+    title = f'(articles.title)'
+    cnn_news = get_articles('cnn') 
+    # BBC_News =get_articles('bbc_news')
+    return render_template('articles.html',title = title, articles = articles,cnn = cnn_news) 
 
 @main.route('/search/<sources_name>')
 def search(main_search):
@@ -36,4 +36,4 @@ def search(main_search):
     sources_name_format = "+".json(sources_name_list)  
     searched_sources = search_sources(sources_name_format)
     title = f'search result for {sources_name}'
-    return render_template('search.html',sources = searched_sources)       
+    return render_template('search.html',sources = searched_sources)  
