0a429336680c2d0754d8d3eae90f73c8316b5b8b
marion vutagwa <marionvutagwa@yahoo.com>
2020-07-08 02:44:45 +0300

    deployment to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -19,9 +19,7 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from .requests import configure_request
+    from .request import configure_request
     configure_request(app)
 
     return app
-    
-    
